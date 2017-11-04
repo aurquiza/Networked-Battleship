6cf7fcfeb1f7565c0aa6c8f3e832660fbfd86233
Alexis <aurqui7@uic.edu>
2017-11-03 23:48:22 -0500

    added constructor for Gui class

--- src/Gui.java
+++ src/Gui.java
@@ -1,10 +1,42 @@
+/*
+ * CS 342 Programming Project 4: Networked-Battleship
+ * 
+ * Names: Alexis Urquiza, Robert Barrera, Muhammad Arsalan Chaudry
+ * NetID: aurqui7, rbarre4, mchaud25
+ * 
+ * - This program is a gui simulation of the game Battleship, where two players can go against each other and attempt
+ *   to sink the other player's fleet. The first player to sink the other player's fleet wins.
+ * 
+ * - This class is the heart of the program where main is located as well as a custom JFrame class that helps 
+ */
 
-public class Gui {
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
+public class Gui extends JFrame
+{
+	private Container mainContainer;
+	private JMenuBar masterBar;
+	
+	private JPanel masterPanel;
+	
+	public Gui()
+	{
+		super("CS 342 Project 4 (Networked-Battleship)");
+		
+		
+		setSize(300,300);
+		setVisible(true);
+	}
+	
+	
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Testing");
+		
+
 	}
 
 }
